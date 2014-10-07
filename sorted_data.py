f582a41b5d5c082d044bd7a725b65a82f15eb4c1
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 17:59:04 -0700

    Sort by Restaurant file

--- sorted_data.py
+++ sorted_data.py
@@ -15,23 +15,28 @@ with open("scores.txt", 'r') as restaurant_ratings:
 #sort keys decending
     #try again?.items can't be used on dict.
     #sorted_restaurants = sorted(restaurant_rating_dict.items(), key = restaurant_rating_dict[0].itemgetter(1), reverse = True)
-    restaurant_names = sorted(restaurant_rating_dict, reverse = True)
+    sort_choice = raw_input("How would you like the restaurant guide to be sorted? By rating or by name?\n>").lower()
+    
+    if ( sort_choice[0] != "r" and sort_choice[0] != "n"):
+        print "you didn't enter name or rating"
+    else:
+        restaurant_names = sorted(restaurant_rating_dict, reverse = True)
 
-    index = 0
-    number_of_keys = len(restaurant_names)
+        index = 0
+        number_of_keys = len(restaurant_names)
 
-    while True:
+        while True:
 #get rating of the restaurant by its name
-        restaurant_name = restaurant_names[index] 
-        rating = restaurant_rating_dict[restaurant_name]
+            restaurant_name = restaurant_names[index] 
+            rating = restaurant_rating_dict[restaurant_name]
 
 # changing the order of printing the values
-        print "%s has a rating of %s." % (restaurant_name, rating)
+            print "%s has a rating of %s." % (restaurant_name, rating)
 
-        index += 1
+            index += 1
 
-        if index == number_of_keys:
-            break
+            if index == number_of_keys:
+                break
 
 
 # on github there is a hint file
