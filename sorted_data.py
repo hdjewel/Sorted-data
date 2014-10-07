f730f61cca151091d17b55965a0911e719acf868
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 17:42:52 -0700

    Sort by Rating file

--- sorted_data.py
+++ sorted_data.py
@@ -6,15 +6,17 @@ with open("scores.txt", 'r') as restaurant_ratings:
     for line in restaurant_ratings:
         aline = line.rstrip().split(':')
 #populate a dictionary with restaurant names as keys with rating values
-        restaurant_rating_dict[aline[0]] = aline[1]
+
+# changed order of retreiving the input
+        restaurant_rating_dict[aline[1]] = aline[0]
 
 
 #get keys from the dictionary
 #sort keys decending
-#restaurant_names = restaurant_rating_dict.keys()
-    restaurant_names = sorted(restaurant_rating_dict.keys())
+    #try again?.items can't be used on dict.
+    #sorted_restaurants = sorted(restaurant_rating_dict.items(), key = restaurant_rating_dict[0].itemgetter(1), reverse = True)
+    restaurant_names = sorted(restaurant_rating_dict, reverse = True)
 
-    #count = 0
     index = 0
     number_of_keys = len(restaurant_names)
 
@@ -23,24 +25,15 @@ with open("scores.txt", 'r') as restaurant_ratings:
         restaurant_name = restaurant_names[index] 
         rating = restaurant_rating_dict[restaurant_name]
 
-#print restaurant names and rating
+# changing the order of printing the values
         print "%s has a rating of %s." % (restaurant_name, rating)
 
-#        print restaurant_name + " ==== " + rating
         index += 1
-        
+
         if index == number_of_keys:
             break
 
 
-"""the following code is for testing only
-        count += 1
-
-
-        if count > 3:
-            break
-
-"""
 # on github there is a hint file
 
 
